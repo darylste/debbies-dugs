b46bf8676e78f3061f75e6f5e4acaa52d35d3398
Daryl Stensland <dstensland5@gmail.com>
2022-10-27 14:18:45 +0100

    eslint configured and husky pre-commit hook set-up

--- atomic/atoms/Test/Test.component.tsx
+++ atomic/atoms/Test/Test.component.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import styles from './Test.module.scss';
 
 const Test = () => {
-	return (
-		<div>
-			<h1 className={styles.heading}>Test Component</h1>
-		</div>
-	);
+  return (
+    <div>
+      <h1 className={styles.heading}>Test Component</h1>
+    </div>
+  );
 };
 
 export default Test;
