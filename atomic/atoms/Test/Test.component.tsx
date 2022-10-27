5c6b807be09cdf5ffc8fc2136a1c7848737c22a1
Daryl Stensland <dstensland5@gmail.com>
2022-10-27 13:19:43 +0100

    file structure created

---
+++ atomic/atoms/Test/Test.component.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Test.module.scss';
+
+const Test = () => {
+	return (
+		<div>
+			<h1 className={styles.heading}>Test Component</h1>
+		</div>
+	);
+};
+
+export default Test;
