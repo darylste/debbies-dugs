b41eaa152a06defc480b6edf27487e645e630a3c
Daryl Stensland <dstensland5@gmail.com>
2022-10-28 14:58:40 +0100

    sanity installed and schemas setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Nav from '../atomic/atoms/Nav/Nav.component';
 import Hero from '../atomic/molecules/Hero/Hero.component';
-
 import ServicesSection from '../atomic/organisms/ServicesSection/ServicesSection.component';
 
 export default function Home() {
