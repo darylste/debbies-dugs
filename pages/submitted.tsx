51fe6e06de26e0f4b147eb7f9adbbacf227f0ad2
Daryl Stensland <dstensland5@gmail.com>
2022-11-18 19:30:49 +0000

    meta tags added to pages

--- pages/submitted.tsx
+++ pages/submitted.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ThankYou from '../atomic/molecules/ThankYou/ThankYou.component';
+import Head from 'next/head';
 
 const Submitted: React.FC = () => {
-  return <ThankYou />;
+  return (
+    <Fragment>
+      <Head>
+        <meta
+          name='title'
+          content="Debbie's Dugs - Thank you for reaching out"
+        />
+        <meta
+          name='description'
+          content='We offer a range of dog walking, boarding and daycare services in Glasgow and are fully insured and K9 first aid-trained.'
+        />
+        <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+        <meta name='language' content='English' />
+        <meta name='author' content='www.daryl-stensland.com/' />
+      </Head>
+      <ThankYou />
+    </Fragment>
+  );
 };
 
 export default Submitted;
