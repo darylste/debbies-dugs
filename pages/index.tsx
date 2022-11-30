b70ede03ae350b8e71718e133eb369df797d7184
Daryl Stensland <dstensland5@gmail.com>
2022-11-30 21:40:23 +0000

    sitemap and social cards addded

--- pages/index.tsx
+++ pages/index.tsx
@@ -54,6 +54,37 @@ const Home: React.FC<IHomeProps> = ({
         <meta name='language' content='English' />
         <meta name='author' content='www.daryl-stensland.com/' />
 
+        <meta property='og:url' content='https://www.debbiesdugs.co.uk' />
+        <meta property='og:type' content='website' />
+        <meta
+          property='og:title'
+          content="Debbie's Dugs - Dog Walking, Boarding and Day Care"
+        />
+        <meta
+          property='og:description'
+          content='We offer a range of dog walking, boarding and daycare services in Glasgow and are fully insured and K9 first aid-trained.'
+        />
+        <meta
+          property='og:image'
+          content='https://scontent.fman1-2.fna.fbcdn.net/v/t39.30808-6/306970613_10217948566499715_6211308581337947387_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=O9m16pzTjVQAX8G7jur&tn=jTTb9YaGF8p5i1p9&_nc_ht=scontent.fman1-2.fna&oh=00_AfCy6eStMmsM2M3nZUSsUVO5gPcDoIAEV_fVpZbvM7FHjA&oe=638BC043'
+        />
+
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta property='twitter:domain' content='debbiesdugs.co.uk' />
+        <meta property='twitter:url' content='https://www.debbiesdugs.co.uk' />
+        <meta
+          name='twitter:title'
+          content="Debbie's Dugs - Dog Walking, Boarding and Day Care"
+        />
+        <meta
+          name='twitter:description'
+          content='We offer a range of dog walking, boarding and daycare services in Glasgow and are fully insured and K9 first aid-trained.'
+        />
+        <meta
+          name='twitter:image'
+          content='https://scontent.fman1-2.fna.fbcdn.net/v/t39.30808-6/306970613_10217948566499715_6211308581337947387_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=O9m16pzTjVQAX8G7jur&tn=jTTb9YaGF8p5i1p9&_nc_ht=scontent.fman1-2.fna&oh=00_AfCy6eStMmsM2M3nZUSsUVO5gPcDoIAEV_fVpZbvM7FHjA&oe=638BC043'
+        />
+
         <link rel='shortcut icon' href='favicon.svg' type='image/x-icon' />
       </Head>
       <Nav />
